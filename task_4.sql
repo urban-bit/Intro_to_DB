59533d6e3ccdf989efce53540262550a8f6898b3
urban-bit <gabrielgabriela57@gmail.com>
2024-07-24 11:04:56 +0200

    information_schema table  scrip

---
+++ task_4.sql
@@ -0,0 +1,16 @@
+-- Use the alx_book_store database
+USE alx_book_store;
+
+-- Query the information_schema to get the full description of the books table
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_KEY, 
+    COLUMN_DEFAULT, 
+    EXTRA 
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books';
