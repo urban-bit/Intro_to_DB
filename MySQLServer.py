ca52f98aeafd782bb4c2a41e8bf8faded4154666
urban-bit <gabrielgabriela57@gmail.com>
2024-07-24 10:51:28 +0200

    book store script import from sql

--- MySQLServer.py
+++ MySQLServer.py
@@ -14,7 +14,7 @@ def create_database():
             cursor = connection.cursor()
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error: {e}")
     finally:
         if connection.is_connected():
@@ -24,3 +24,4 @@ def create_database():
 
 if __name__ == "__main__":
     create_database()
+
